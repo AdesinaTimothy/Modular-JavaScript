575ba4c2c71d0801229bee81c45c3076a838edc7
Timothy Adesina <timobest121@gmail.com>
2024-10-10 20:55:15 +0100

    Inital Commit

---
+++ mod.js
@@ -0,0 +1,11 @@
+document.getElementById("body");
+import form from "./form.js"
+
+function mainContainer () {
+    let mainContainerDiv = document.createElement("div");
+    mainContainerDiv.classList.add("container");
+    body.append(mainContainerDiv);
+    mainContainerDiv.append(form())
+}
+
+mainContainer();
